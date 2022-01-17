a0171339cb76a58e39925b8358c6a79ca20d97c8
adam-dalbello <adam.dalbello@gmail.com>
2022-01-17 15:58:02 +0000

    Add files via upload

---
+++ scrts scrpt.R
@@ -0,0 +1,21 @@
+#caesar
+
+
+empty_vector <- vector('list', 9) 
+
+
+for (i in 3:11) { empty_vector[i-2]  <- get(ls()[i]) }
+
+
+# keeper
+keys <- dplyr::bind_cols(
+                            data.frame(
+                                          item = ls()[3:11]
+                                ),
+                                                        plyr::ldply(empty_vector)
+  )
+
+#write.csv(keys, 'keys.csv', row.names = FALSE)
+#read.csv('keys.csv')
+
+
